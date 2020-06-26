19cef53aa6a0ef9f64b328599b88620c6b89a363
tpsef <pseftelistheo@gmail.com>
2020-06-26 23:20:52 +0300

    Install bootstrap

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -7,12 +7,20 @@
     <title>Company</title>
 
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/form.js') }}"></script>
 </head>
 <body>
 <h1 class='display' >Form 1</h1>
+
+
+<form method="POST" action="/profile">
+@csrf
+
+</form>
+
 
 </body>
 </html>
