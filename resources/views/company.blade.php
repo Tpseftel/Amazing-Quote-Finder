1419daa025d5d12b0a43951d749bd837113307f6
tpsef <pseftelistheo@gmail.com>
2020-06-27 23:30:44 +0300

    Update company.blade.php

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -20,14 +20,14 @@
 <body>
     <div class="container">
         <h1 class=''>Form 1</h1>
-        <form class="needs-validation" novalidate >
-
+        <form class="needs-validation" novalidate action="/history"  method="post">
+          @csrf
             <!-- Company Symbol -->
             <div class="form-row">
                 <div class="col-md-4 mb-3">
                     <label for="company-symbol" class="text-capitalize font-weight-bold" >Company Symbol:</label>
 
-                    <select class="selectpicker form-control" id="company-symbol" data-live-search="true" style="border:1px solid blue;"required>
+                    <select class="selectpicker form-control" value="{{old('company_symbol')}}" name="company_symbol" data-live-search="true" style="border:1px solid blue;"required>
                         @foreach($companies as $company)
                             <option data-tokens="ketchup mustard" value= "{{$company['Symbol']}}" >{{$company['Company Name']}} </option>
                         @endforeach
@@ -42,8 +42,8 @@
             <div class="form-row">
                 <div class="col-md-4 mb-3">
                 <label for="start-date" class="text-capitalize font-weight-bold">Start Date: </label>
-                    <input type="text"  id="start-date" class="form-control datepicker" required>
-                    <div class="invalid-feedback">
+                    <input type="text" value="{{old('start_date')}}"  name="start_date" class="form-control datepicker" required>
+                    <div class="invalid-feedback" id='start-error'>
                         Pleaze choose the Start Date.
                     </div>
                 </div>
@@ -52,8 +52,8 @@
             <!-- End Date -->
             <div class="form-row">
                 <div class="col-md-4 mb-3">
-                <label for="end-date" class="  text-capitalize font-weight-bold">End Date: </label>
-                    <input type="text" id="end-date" class="form-control datepicker" required>
+                <label for="end-date" class=" text-capitalize font-weight-bold">End Date: </label>
+                    <input type="text" name="end_date" value="{{old('end_date')}}" class="form-control datepicker" required>
                     <div class="invalid-feedback">
                           Pleaze choose the End Date.
                     </div>
@@ -65,7 +65,7 @@
                 <div class="col-md-4 mb-3">
                     <label for="email" class="font-weight-bold text-capitalize">Email:</label>
                     <br>
-                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"required>
+                    <input type="email" value="{{old('email')}}" class="form-control" name="email" aria-describedby="emailHelp" placeholder="Enter email"required>
                     <div class="invalid-feedback">
                         Pleaze enter your email.
                     </div>
@@ -74,6 +74,11 @@
 
             <button class="btn btn-primary" id="submit-btn" type="submit">Submit form</button>
         </form>
+        @if($errors->has('start_date'))
+            @foreach ($errors->all() as $error)
+                <div>{{ $error }}</div>
+            @endforeach
+    @endif
 
     </div>
 </body>
