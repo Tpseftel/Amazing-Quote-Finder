538cb6e2d3b217979ee0ce8fbc526ba83058f33a
tpsef <pseftelistheo@gmail.com>
2020-06-27 17:30:19 +0300

    Update form with error feedback

--- public/js/form.js
+++ public/js/form.js
@@ -1,9 +1,23 @@
-
-onload = function(){
-    console.log('Hello World');
-};
-
 $( function() {
+    // Set up Form Validation
+   (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+          // Fetch all the forms we want to apply custom Bootstrap validation styles to
+          var forms = document.getElementsByClassName('needs-validation');
+          // Loop over them and prevent submission
+          var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+              if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+              }
+              form.classList.add('was-validated');
+            }, false);
+          });
+        }, false);
+    })();
+
     // SetUp Date Picker
     $( ".datepicker" ).datepicker({
         dateFormat: 'yy-mm-dd',
@@ -12,18 +26,35 @@ $( function() {
         yearRange: "-100:+0", 
         currentText: "Now"
     });
-
     $('.selectpicker').selectpicker();
 
-    $('#submit-btn').click(function () {
-        const start_date = $( "#start-date" ).datepicker( "getDate" );
-        const end_date = $( "#end-date" ).datepicker( "getDate" );
-        const company_symbol = $('#company-symbol').val();
-        console.log('Start Date:' + start_date );
-        console.log('End Date:' + end_date );
-        console.log('Copmany Symbol:' + company_symbol );
 
-        console.log('Moment Now:' + moment(start_date).format('YYYY-MMMMM-dd'));
+
+
+    function validateStartDate(){
+        const startDate = $('start-date').val();
+        if(!startDate){
+            alert('No start day');
+        }
+    }
+
+
+
+
+
+    $('#submit-btn').click(function () {
+        validateStartDate();
+        // const start_date = $( "#start-date" ).datepicker( "getDate" );
+        // const end_date = $( "#end-date" ).datepicker( "getDate" );
+        // const company_symbol = $('#company-symbol').val();
+        // const email = $('#email').val();
+
+        // console.log('Start Date:' + start_date );
+        // console.log('End Date:' + end_date );
+        // console.log('Copmany Symbol:' + company_symbol );
+        // console.log('Email:' + email );
+
+        // // console.log('Moment Now:' + moment(start_date).format('YYYY-MMMMM-dd'));
     });
 
 
--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -20,25 +20,20 @@
 <body>
     <div class="container">
         <h1 class=''>Form 1</h1>
-
-        <form>
+        <form class="needs-validation" novalidate >
 
             <!-- Company Symbol -->
             <div class="form-row">
                 <div class="col-md-4 mb-3">
                     <label for="company-symbol" class="text-capitalize font-weight-bold" >Company Symbol:</label>
 
-                    <select class="selectpicker form-control" id="company-symbol" data-live-search="true" style="border:1px solid blue;">
+                    <select class="selectpicker form-control" id="company-symbol" data-live-search="true" style="border:1px solid blue;"required>
                         @foreach($companies as $company)
                             <option data-tokens="ketchup mustard" value= "{{$company['Symbol']}}" >{{$company['Company Name']}} </option>
                         @endforeach
                     </select>
-
-
-<br>
-
-                    <div class="valid-feedback">
-                        Looks good!
+                    <div class="invalid-feedback">
+                        Field is required
                     </div>
                 </div>
             </div>
@@ -47,34 +42,38 @@
             <div class="form-row">
                 <div class="col-md-4 mb-3">
                 <label for="start-date" class="text-capitalize font-weight-bold">Start Date: </label>
-                    <input type="text"  id="start-date" class="form-control datepicker">
+                    <input type="text"  id="start-date" class="form-control datepicker" required>
+                    <div class="invalid-feedback">
+                        Pleaze choose the Start Date.
+                    </div>
                 </div>
             </div>
-
+           
             <!-- End Date -->
             <div class="form-row">
                 <div class="col-md-4 mb-3">
                 <label for="end-date" class="  text-capitalize font-weight-bold">End Date: </label>
-                    <input type="text" id="end-date" class="form-control datepicker">
+                    <input type="text" id="end-date" class="form-control datepicker" required>
+                    <div class="invalid-feedback">
+                          Pleaze choose the End Date.
+                    </div>
                 </div>
             </div>
-
+           
+            <!-- Email  -->
             <div class="form-row">
                 <div class="col-md-4 mb-3">
                     <label for="email" class="font-weight-bold text-capitalize">Email:</label>
                     <br>
-                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"required>
                     <div class="invalid-feedback">
-                        Looks good!
+                        Pleaze enter your email.
                     </div>
                 </div>
             </div>
-            
-      
 
             <button class="btn btn-primary" id="submit-btn" type="submit">Submit form</button>
         </form>
-
 
     </div>
 </body>
