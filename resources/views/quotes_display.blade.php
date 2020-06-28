8636300c674fa193f703c5db27a6243271b3c402
tpsef <pseftelistheo@gmail.com>
2020-06-28 12:00:05 +0300

    Create quote table
     Populate with data

--- resources/views/quotes_display.blade.php
+++ resources/views/quotes_display.blade.php
@@ -14,10 +14,40 @@
 </head>
 <body>
 
+<table id="table_quotes" class="display table">
+    <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Date</th>
+      <th scope="col">Open</th>
+      <th scope="col">High</th>
+      <th scope="col">Low</th>
+      <th scope="col">Close</th>
+      <th scope="col">Volune</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($quotes['prices'] as $key => $quote)
+        <tr>
+            <th scope="row"> {{ $key }} </th>
+            <td> {{ $quote['date'] }} </td>
+            <td> {{ $quote['open'] }} </td>
+            <td> {{ $quote['high'] }} </td>
+            <td> {{ $quote['low'] }} </td>
+            <td> {{ $quote['close'] }} </td>
+            <td> {{ $quote['volume'] }} </td>
+        </tr>
+    @endforeach 
+    
+  </tbody>
+</table>
+
+
+
+{{-- 
  @foreach($quotes['prices'] as $key => $quote)
     {{$quote['open']}}
-     
- @endforeach
+ @endforeach --}}
     
 </body>
 </html>
