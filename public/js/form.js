d608bf37e25318695db7277954ccda0dd6295947
tpsef <pseftelistheo@gmail.com>
2020-06-28 22:58:59 +0300

    Update validation

--- public/js/form.js
+++ public/js/form.js
@@ -1,10 +1,7 @@
 // const moment = require("moment");
-
 $( function() {
-    // Set up Form Validation
-    
+  // Set up Form Validation
    (function() {
-        'use strict';
         window.addEventListener('load', function() {
           // Fetch all the forms we want to apply custom Bootstrap validation styles to
           var forms = document.getElementsByClassName('needs-validation');
@@ -26,47 +23,59 @@ $( function() {
         dateFormat: 'yy-mm-dd',
         changeMonth: true,
         changeYear: true,
-        yearRange: "-100:+0", 
-        currentText: "Now"
+        yearRange: "-100:+0"
     });
     $('.selectpicker').selectpicker();
 
     // Set Symbol Disabled Input
     $('#symbol-input').val( $('#company-select').val() );
-
-
-
-
-    // function validateStartDate(){
-    //     let startDate = $('#start-date');
-    //     // startDate = moment(startDate);
-    //     let error_text = '';
-    //     if(startDate.val()>3){
-    //        document.getElementById('start-date').setCustomValidity('Hello');
-    //        document.getElementById('start-date').reportValidity();
-    //     // document.getElementById("submit-btn").click();
-    //     }
-    // }
-
-
+    // Set company symbol
     $('#company-select').change(function(){
       $('#symbol-input').val( $(this).val() );
     });
 
 
     $('#submit-btn').click(function () {
-        const start_date = $( "#start-date" ).datepicker( "getDate" );
-        const end_date = $( "#end-date" ).datepicker( "getDate" );
-        const company_symbol = $('#company-symbol').val();
-        const email = $('#email').val();
+        const start_date = $("#start-date").val();
+        const end_date = $("#end-date").val();
+        const company_symbol = $('#company-select').val();
+        const email = $('#email-input').val();
+
+
+        functionlowerThanToday()
+
+        
+
+
+        function isValidDate(dateString) {
+            var regEx = /^\d{4}-\d{2}-\d{2}$/;
+            if(!dateString.match(regEx)) return false;  // Invalid format
+            var d = new Date(dateString);
+            var dNum = d.getTime();
+            if(!dNum && dNum !== 0) return false; // NaN value, Invalid date
+            return d.toISOString().slice(0,10) === dateString;
+          }
+
+
+
+
+
+
 
         console.log('Start Date:' + start_date );
         console.log('End Date:' + end_date );
         console.log('Copmany Symbol:' + company_symbol );
         console.log('Email:' + email );
 
-        // // console.log('Moment Now:' + moment(start_date).format('YYYY-MMMMM-dd'));
     });
+
+
+
+   
+
+
+
+
 
 
 });
--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -25,7 +25,7 @@
             <!-- Company Symbol -->
             <div class="form-row">
                 <div class="col-md-4 mb-3">
-                    <label for="company-symbol" class="text-capitalize font-weight-bold" >Company Symbol:</label>
+                    <label for="company-select" class="text-capitalize font-weight-bold" >Company Name:</label>
 
                     <select class="selectpicker form-control {{$errors->has('company_symbol') ? 'is-invalid' :''}}" id="company-select" value="{{old('company_symbol')}}" name="company_symbol" data-live-search="true" style="border:1px solid blue;"required>
                         @foreach($companies as $company)
@@ -54,8 +54,8 @@
             <div class="form-row">
                 <div class="col-md-4 mb-3">
                 <label for="start-date" class="text-capitalize font-weight-bold">Start Date: </label>
-                    <input type="text" value="{{old('start_date')}}"  name="start_date"
-                     class=" {{$errors->has('start_date') ? 'is-invalid' :''}} form-control datepicker" required>
+                    <input type="text"  placeholder="yyyy-mm-dd" value="{{old('start_date')}}" id="start-date" name="start_date"
+                     class=" {{$errors->has('start_date') ? 'is-invalid' :''}}  form-control datepicker" required>
                     <div class="invalid-feedback" id='start-error'>
                         @if ($errors->has('start_date'))
                            {{ $errors -> first('start_date')}}
@@ -70,7 +70,7 @@
             <div class="form-row">
                 <div class="col-md-4 mb-3">
                 <label for="end-date" class=" text-capitalize font-weight-bold">End Date: </label>
-                    <input type="text" name="end_date" value="{{old('end_date')}}" 
+                    <input type="text"  placeholder="yyyy-mm-dd" name="end_date" id="end-date" value="{{old('end_date')}}" 
                         class=" {{$errors->has('end_date') ? 'is-invalid' :''}}  form-control datepicker" required>
                     <div class="invalid-feedback">
                         @if ($errors->has('end_date'))
@@ -87,7 +87,7 @@
                 <div class="col-md-4 mb-3">
                     <label for="email" class="font-weight-bold text-capitalize">Email:</label>
                     <br>
-                    <input type="email" value="{{old('email')}}" class="{{$errors->has('email') ? 'is-invalid' :''}} form-control" name="email" aria-describedby="emailHelp" placeholder="Enter email"required>
+                    <input type="email" id="email-input" value="{{old('email')}}" class="{{$errors->has('email') ? 'is-invalid' :''}} form-control" name="email" aria-describedby="emailHelp" placeholder="Enter email"required>
                     <div class="invalid-feedback">
                     @if ($errors->has('email'))
                            {{ $errors -> first('email')}}
