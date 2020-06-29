0eb716f56b0c43c132db0b2adcd4cd7cca38e395
tpsef <pseftelistheo@gmail.com>
2020-06-29 11:30:20 +0300

    Update Form display

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -12,16 +12,18 @@
     <!-- Latest compiled and minified CSS -->
    
 </head>
-<input type="text" class="form-control">
-
 <body>
-    <div class="container">
-        <h1 class=''>Form 1</h1>
-        <form class="needs-validation" novalidate action="/history"  method="post">
+    <div class="container-fluid ">
+        
+        <div class="row justify-content-center">
+        
+            <div class="col-4 ">
+            <h1 class='text-center'>Company</h1>
+            <form class="needs-validation"  novalidate action="/history"  method="post">
           @csrf
             <!-- Company Name -->
             <div class="form-row">
-                <div class="col-md-4 mb-3">
+                <div class="col-md-12 mb-3">
                     <label for="company-select" class="text-capitalize font-weight-bold" >Company Name:</label>
 
                     <select class="selectpicker form-control {{$errors->has('company_symbol') ? 'is-invalid' :''}}" id="company-select" value="{{old('company_symbol')}}" name="company_symbol" data-live-search="true" style="border:1px solid blue;"required>
@@ -38,7 +40,7 @@
             </div>
 
             <div class="form-row">
-                <div class="col-md-4 mb-3">
+                <div class="col-md-12 mb-3">
                     <label for="symbol" class="text-capitalize font-weight-bold" >Company Symbol:</label>
                     <input type="text" value="{{old('company_symbol')}}" id="symbol-input"  name="symbol" class="form-control" disabled>
                     
@@ -47,9 +49,9 @@
 
             <!--Start Date  -->
             <div class="form-row">
-                <div class="col-md-4 mb-3">
+                <div class="col-md-12 mb-3">
                 <label for="start-date" class="text-capitalize font-weight-bold">Start Date: </label>
-                    <input type="text"  placeholder="yyyy-mm-dd" value="{{old('start_date')}}" id="start-date" name="start_date"
+                    <input type="text" autocomplete="off"  placeholder="yyyy-mm-dd" value="{{old('start_date')}}" id="start-date" name="start_date"
                      class=" {{$errors->has('start_date') ? 'is-invalid' :''}}  form-control datepicker" required>
                     <div class="invalid-feedback start-date-error" >
                         @if ($errors->has('start_date'))
@@ -61,9 +63,9 @@
            
             <!-- End Date -->
             <div class="form-row">
-                <div class="col-md-4 mb-3">
+                <div class="col-md-12 mb-3">
                 <label for="end-date" class=" text-capitalize font-weight-bold">End Date: </label>
-                    <input type="text"  placeholder="yyyy-mm-dd" name="end_date" id="end-date" value="{{old('end_date')}}" 
+                    <input type="text" autocomplete="off"  placeholder="yyyy-mm-dd" name="end_date" id="end-date" value="{{old('end_date')}}" 
                         class=" {{$errors->has('end_date') ? 'is-invalid' :''}}  form-control datepicker" required>
                     <div class="invalid-feedback end-date-error">
                         @if ($errors->has('end_date'))
@@ -75,7 +77,7 @@
            
             <!-- Email  -->
             <div class="form-row">
-                <div class="col-md-4 mb-3">
+                <div class="col-md-12 mb-3">
                     <label for="email" class="font-weight-bold text-capitalize">Email:</label>
                     <br>
                     <input type="email" id="email-input" value="{{old('email')}}" class="{{$errors->has('email') ? 'is-invalid' :''}} form-control" name="email" aria-describedby="emailHelp" placeholder="Enter email"required>
@@ -89,8 +91,10 @@
 
             <button class="btn btn-primary" id="submit-btn" type="submit">Submit form</button>
         </form>
+            </div>
+        </div>
+        
     </div>
-
 
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/validation.js') }}"></script>
